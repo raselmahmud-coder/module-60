7614099e6af72fdf0307278b2ed0fc5f1b24af68
RASEL MAHMUD <raselmahmud98262@gmail.com>
2022-04-13 18:22:32 +0800

    process.env added

--- src/Pages/Sign Up/SignUp.js
+++ src/Pages/Sign Up/SignUp.js
@@ -1,26 +1,44 @@
 import React from "react";
 import { Button, Form, FormText } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import auth from "../../firebase-init";
 
 const SignUp = () => {
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
   const handleForm = (e) => {
     e.preventDefault();
     const name = e.target.name.value;
     const email = e.target.email.value;
     const password = e.target.password.value;
-    console.log(email);
+    createUserWithEmailAndPassword(email, password);
+
+    if (error) {
+      return (
+        <div>
+          <p>Error: {error.message}</p>
+        </div>
+      );
+    }
+    if (loading) {
+      return <p>Loading...</p>;
+    }
+    if (user) {
+      return (
+        <div>
+          <p>Registered User: {user.email}</p>
+        </div>
+      );
+    }
   };
   return (
     <>
       <Form className="w-25 mx-auto" onSubmit={handleForm}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Name</Form.Label>
-          <Form.Control
-            name="name"
-            type="text"
-            placeholder="Enter name"
-            required
-          />
+          <Form.Control name="name" type="text" placeholder="Enter name" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
